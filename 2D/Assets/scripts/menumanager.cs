b4284b372f6206252f753432b719ee1ee6f3784e
a0975660266 <71679957+a0975660266@users.noreply.github.com>
2020-11-03 16:02:55 +0800

    新增碰撞器

--- 2D/Assets/scripts/menumanager.cs
+++ 2D/Assets/scripts/menumanager.cs
@@ -1,11 +1,16 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class menumanager : MonoBehaviour
 {
-void startgame()
-    { 
-    }
-void quitgame()
+public void startgame()
     {
+        print("開始遊戲");
+        SceneManager.LoadScene("關卡1");
+    }
+    public void quitgame()
+    {
+        print("結束遊戲");
+        Application.Quit();
     }
 }
